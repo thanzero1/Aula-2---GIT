da8b21315e6a22d495fcccf3a1b3e9895af7c1e1
NathanGarcia <nathangarcia750@gmail.com>
2025-11-14 17:25:44 -0300

    Correção de bugs

--- viagem.js
+++ viagem.js
@@ -3,7 +3,8 @@ let velocidadeMedia = 85;
 let consumoMedio = 12 ;
 let precoGasolina = 6.17
 let precoEtanol = 4.17
-
+let tipoCombustivel = 0 
+// se retornar 0 é Etanol, se retornar 1 é Gasolina
 
 let distancia = (horas * velocidadeMedia)
 console.log(`A distância total da viagem é de ${distancia}km`)
@@ -15,4 +16,12 @@ console.log(`o consumo total de litros será ${(consumoTotal).toLocaleString('pt
 console.log(`O custo para abastecer com Etanol será de ${(consumoTotal * precoEtanol).toLocaleString('pt-BR',{style:'currency', currency: 'BRL'})}`)
 console.log(`O custo para abastecer com Gasolina será de ${ (consumoTotal * precoGasolina).toLocaleString('pt-BR',{style:'currency', currency: 'BRL'})}`)
 
+
+if(tipoCombustivel == 0){
+    console.log("Combustível é etanol")
+} else if(tipoCombustivel > 0) {
+    console,log("Combustivel é Gasolina")
+} else {
+    console.log("selecione o Combustível")
+}
 
