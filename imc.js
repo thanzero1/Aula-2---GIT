9e5c418fdf8d34c5bc2859517f782a82316c188f
NathanGarcia <nathangarcia750@gmail.com>
2025-11-14 15:32:27 -0300

    novos arquivos adicionados

--- imc.js
+++ imc.js
@@ -1,3 +1,18 @@
 let nome = "nathan", altura = 1.80, peso = 81, imc = peso / (altura **2);
 
-console.log(`O Imc do ${nome} é de ${imc.toFixed(1)}`)
+
+
+
+if(imc < 18.5 ) {
+    console.log(`O ${nome} está abaixo do peso normal`)
+} else if (imc >= 18.5 && imc < 24.9 ) {
+    console.log(`O ${nome} está no peso normal`)
+} else if (imc >= 25.0 && imc < 29.9) {
+    console.log(`O ${nome} está com excesso de peso`)
+} else if (imc >= 30.0 && imc < 34.9) {
+    console.log(`O ${nome} está com obesidade classe 1`)
+} else if (imc >= 35.0 && imc < 39.9){
+    console.log(`O ${nome} está com obesidade classe 2`)
+} else {
+    console.log(`O ${nome} está com obesidade classe 3`)
+}1000/12
---
+++ viagem.js
@@ -0,0 +1,11 @@
+let tempoGasto = 8;
+let velocidadeMedia = 85;
+let consumoMedio = 12 ;
+let calculo = (tempoGasto * velocidadeMedia)
+
+let resultado = calculo / 12;
+
+
+console.log(resultado.toFixed(3))
+
+
